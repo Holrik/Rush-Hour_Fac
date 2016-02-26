e7169544656aa7e3975150c6d65293a6627afc96
johngp <jonathanbesse33300@gmail.com>
2016-02-26 14:50:32 +0100

    Update game.c
    amélioration du code

--- game.c
+++ game.c
@@ -157,21 +157,36 @@ bool play_move(game g, int piece_num, dir d, int distance){
 		return NULL ;
 	}
 	copy_piece(game_piece(g, piece_num), p) ;
-	for (i = 0 ; i < distance ; i++) {
+	/*for (i = 0 ; i < distance ; i++) {
 		move_piece(p, dir, 1) ; // A chaque case traversée par la copie de notre pièce,
 		int i = 0;
 		while (i < piece_num) { // on vérifie si elle intersect une autre pièce
-			if (intersect(p, piece_num))
+			if (intersect(p, game_piece(g,i)))
 				return false ;
 			i++ ;
 		}
 		i++ ; // On évite de vérifier si la copie croise la pièce dont elle est issue
 		while (i < game_nb_pieces(g)) {
-			if (intersect(p, piece_num))
+			if (intersect(p, game_piece(g, i)))
 				return false ;
 			i++ ;
 		}
 	}
+	*/
+
+	//Solution plus efficace
+	for (i = 0; i < distance; i++) {
+		move_piece(p, dir, 1); // A chaque case traversée par la copie de notre pièce,
+		int i = 0;
+		while (i < game_nb_pieces(g)) {
+			if (intersect(p, game_piece(g, i)) && i != piece_num)
+				return false;
+			i++;
+		}
+	}
+
+
+
 	delete_piece(p) ;
 	// Après les 3 vérifications, si tout est bon, on déplace la pièce
 	move_piece(game_piece(g, piece_num), dir, distance) ;
