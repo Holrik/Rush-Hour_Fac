03b5f20390a97fade9ca703f4d5c1535def12ec9
johngp <jonathanbesse33300@gmail.com>
2016-02-28 14:17:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,39 @@
 
 void main (){
 game g = new_game_hr (); // aremplir avec ce qu'il faut
-char * buf;
+char * nums;
+int numi;
+char * direc;
+int direci;
+char * dists;
+int disti;
 while (game_over_hr(g))
     {
-       fgets(buf, 10, stdin); //a tester
+        debut:
+        printf("Veuillez entrer le numero de la voiture:");
+       fgets(nums, 2, stdin); //a tester
+       numi= atoi(nums);
+        printf("Veuillez entrer la direction du déplacement:");
+       fgets(direc, 10, stdin); //a tester printf("Veuillez entrer le numero de la voiture:");
 
+           
+        if(direc == "UP"){
+            direci=0;
+        }else if(direc == "DOWN"){
+            direci=2;
+        }else if(direc == "LEFT"){
+            direci=1;
+        }else if(direc == "RIGHT"){
+            direci=3;
+        }else {
+            goto debut;
+        }
+            printf("Veuillez entrer le nombre de déplacement:");
+           fgets(dists, 1, stdin); 
+            disti= atoi(deps);
+            
+            play_move(g, numi,direci,depi);
     }
-
+    
 }
+
