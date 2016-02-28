2476bc600ae0e5db91700607013eaa6bd0800124
johngp <jonathanbesse33300@gmail.com>
2016-02-28 13:22:14 +0100

    Create main.c
    pour voir

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "piece.h"
+#include "game.h"
+
+
+void main (){
+bool trouve = false;
+char * buf;
+while (!trouve)
+    {
+       fgets(buf, 10, stdin); //a tester
+
+    }
+
+}
