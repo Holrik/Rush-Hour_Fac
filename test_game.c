ed6d1880c88e4877a70c0e40acae5f5f4d653eda
johngp <jonathanbesse33300@gmail.com>
2016-02-29 12:45:07 +0100

    Update test_game.c
    A tester, j'ai essayer de comprendre ce que tu voulais

--- test_game.c
+++ test_game.c
@@ -9,45 +9,45 @@
 bool test_new_game (){
 	Erreur : A la ligne suivante, il faut renvoyer un POINTEUR sur une structure
 	et non pas une structure directement(idem dans tout le programme)
-	game g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
-	bool b = (g != NULL);
-	delete_game(g) ;
+	game *g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
+	bool b = (*g != NULL);
+	delete_game(*g) ;
 	return b;
 }
 
 
 //retourne vrai si le game a bien été supprimé
 bool test_delete_game(){
-	game g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
-	delete_game(g);
-	return g == NULL;
+	game *g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
+	delete_game(*g);
+	return *g == NULL;
 }
 
 
 //retourne vrai si la copie est bien la copie exact de l'original 
 bool test_copy_game(){
-	game gsrc = new_game_hr (3, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true),new_piece_rh(3,1,true,false)});
-	game gdst = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
-	copy_game (gsrc,gdst);
-	if (gdst->nb_pieces != gsrc->nb_pieces){
+	game *gsrc = new_game_hr (3, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true),new_piece_rh(3,1,true,false)});
+	game *gdst = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
+	copy_game (*gsrc,*gdst);
+	if ((*gdst)->nb_pieces != (*gsrc)->nb_pieces){
 		return false;
 	}
-	for (int i = 0 ; i < gdst->nb_pieces; i++) { 
-		if(*(gdst->pieces +i) != *(gsrc->pieces+i)) {
+	for (int i = 0 ; i < (*gdst)->nb_pieces; i++) { 
+		if(*((*gdst)->pieces +i) != *((*gsrc)->pieces+i)) {
 	  		return false;
 		}
 	}
-	bool b = (gdst->nb_moves ==gsrc->nb_moves);
-	delete_game(gdst);
-	delete_game(gsrc);
+	bool b = ((*gdst)->nb_moves ==(*gsrc)->nb_moves);
+	delete_game(*gdst);
+	delete_game(*gsrc);
 	return b ;
 }
 
 // retourne vrai si le nombre de pièces retourné par la fonction est correct
 bool test_game_nb_pieces(){
-	game g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
-	bool b = (2==game_nb_pieces(g));
-	delete_game(g);
+	game *g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
+	bool b = (2==game_nb_pieces(*g));
+	delete_game(*g);
 	return b ;
 }
 
@@ -55,18 +55,18 @@ bool test_game_nb_pieces(){
 // retourne true si la pièce est bien récupérée
 bool test_game_piece(){
 	piece test = new_piece_rh(0,3,true,true);
-	game g = new_game_hr (2, {test, new_piece_rh(2,4,false, true)});
-	bool b = (test == game_piece(g, 0));
-	delete_game(g);
+	game *g = new_game_hr (2, {test, new_piece_rh(2,4,false, true)});
+	bool b = (test == game_piece(*g, 0));
+	delete_game(*g);
 	return b ;
 }
 
 
 // retourne true si le game_over_hr return true pour la voiture rouge ayant les coordonnées (4, 3)
 bool test_game_over_hr(){
-	game g = new_game_hr (2, {test, new_piece_rh(4,3,false, true)});
-	bool b = game_over_hr(g) ;
-	delete_game(g) ;
+	game *g = new_game_hr (2, {test, new_piece_rh(4,3,false, true)});
+	bool b = game_over_hr(*g) ;
+	delete_game(*g) ;
 	return b ;
 }
 
@@ -84,10 +84,10 @@ bool test_play_move(){
 
 // retourne true si le nb de mouvements retournés correspond au nb de moves
 bool game_nb_moves() {
-	game g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
-	bool b = (game_nb_moves(g) == 0) ;
-	play_move(g, 0, RIGHT, 2) ;
-	b = b && (game_nb_moves(g) == 2) ;
+	game *g = new_game_hr (2, {new_piece_rh(0,3,true,true), new_piece_rh(2,4,false, true)});
+	bool b = (game_nb_moves(*g) == 0) ;
+	play_move(*g, 0, RIGHT, 2) ;
+	b = b && (game_nb_moves(*g) == 2) ;
 	return b ;
 }
 
