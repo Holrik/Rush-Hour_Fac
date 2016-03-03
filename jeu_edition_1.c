f99cd1273a3dc1010a9d188ba9ab39c539386cf0
Emmanuel Chene <echene001@emi.u-bordeaux1.fr>
2016-03-03 10:59:44 +0100

    mon message

--- jeu_edition_1.c
+++ jeu_edition_1.c
@@ -36,37 +36,112 @@ void afficher(cgame g){
        }
     }
     
-    
-    printf("  ");
-    for(int i = 0; i < TAILLE_PLATEAU; i++){
-     printf("   %d" + i);
-    }
-    printf("\n");
-    
      for(int i = TAILLE_PLATEAU-1; i >= 0; i--){
         printf("   ");
+	
         for(int j=0; j <TAILLE_PLATEAU; j++){
             printf("****");
         }
-        printf("*\n");
-        printf("%d: |", i);
+	
+        printf("*\n%d: |", i);
+	
         for(int j=0; j <TAILLE_PLATEAU; j++){
             if(tab[i][j]==-1){
                 printf("   |");
             }else{
                 printf(" %d |", tab[i][j]);
             }
-        } 
+        }
+	
         if (i == 3)
             printf(" ->");
         printf("\n");
     }
     printf("   *");
-     for(int i=0; i <TAILLE_PLATEAU; i++){
+    
+    for(int i=0; i <TAILLE_PLATEAU; i++){
      printf("****");
     }
-    printf("\n");
+    
+    printf("\n  ");
+    
+    for(int i = 0; i < TAILLE_PLATEAU; i++){
+      printf("   %d", i);
+    }
+    
+    printf("\n\n");
 }
+
+//-----------------------------------------------------------------//
+
+int choixVoiture(){
+
+  char nums[4] = "";
+  printf("Veuillez entrer le numero de la voiture: ");
+
+  while (fgets(nums, 4, stdin) == NULL
+	 || (!isdigit(nums[0]) || nums[1] != '\n') // Ne permet qu'un chiffre dans le nombre de la voiture - AA
+	 || atoi(nums) < 0
+	 || atoi(nums) >= game_nb_pieces(g))
+  {
+	  printf("Raté ! Veuillez entrer un numéro entre 0 et %d : ", game_nb_pieces(g)-1);
+  }
+
+  return atoi(nums);
+  
+}
+
+//-----------------------------------------------------------------//
+
+int deplacementVoiture(){
+
+  char direc[10] = ""; // UP/DOWN/LEFT/RIGHT
+  
+  printf("Veuillez entrer la direction du déplacement: ");
+
+  while(fgets(direc, 10, stdin) == NULL
+	|| strcmp(direc, "UP")!= 0
+        || strcmp(direc, "DOWN")!= 0
+        || strcmp(direc, "LEFT")!= 0
+        || strcmp(direc, "RIGHT")!= 0)
+  {
+    printf("Direction incorrecte. Veuillez entrer UP/DOWN/LEFT/RIGHT.\n");
+  }
+
+  if(strcmp(direc, "UP")){
+    return 0;
+  } else if(strcmp(direc, "LEFT")){
+    return 1;
+  } else if(strcmp(direc, "DOWN")){
+    return 2;
+  } else {
+    return 3;
+  }
+
+  return -1;
+}
+
+//-----------------------------------------------------------------//
+
+int distanceDeplacement(){
+
+  char dists[4] = ""; // Distance déplacement en STRING
+
+  printf("Veuillez entrer la distance du déplacement: ");
+
+  // TODO - Vérifier entrées utilisateur
+  while(fgets(dists, 4, stdin) == NULL
+	|| (!isdigit(dists[0])
+	|| dists[1] != '\n') // Ne permet qu'un chiffre dans le nombre de la distance (A améliorer ?)
+	|| atoi(dists) < 0
+	|| atoi(dists) >= game_nb_pieces(g))
+  {
+    printf("Distance incorrecte. Veuillez entrer une distance de déplacement entre 1 et %d: ", TAILLE_PLATEAU);
+  }
+  return atoi(dists); 
+}
+
+//-----------------------------------------------------------------//
 
 // Manque la création et l'affichage!!
 int main (){
@@ -76,68 +151,14 @@ int main (){
 	*(p+2) = new_piece_rh(3,1,false, false) ;
 	
   game g = new_game_hr (3, p); // à remplir avec ce qu'il faut | TODO
-  
-  char nums[4] = ""; // Numéro voiture à déplacer en STRING
-  int numi; // Numéro voiture à déplacer en INT
-  
-  char direc[10] = ""; // UP/DOWN/LEFT/RIGHT
-  int direci = -1; // Direction de la voiture à partir de direc
-  
-  char dists[4] = ""; // Distance déplacement en STRING
-  int disti; // Distance déplacement en INT
+
   
   afficher(g);
 
 // Tant que ce n'est pas la fin, on fait le corps du programme
 while (!game_over_hr(g))
     {
-        printf("Veuillez entrer le numero de la voiture: ");
-
-	while (fgets(nums, 4, stdin) == NULL
-	       || (!isdigit(nums[0]) || nums[1] != '\n') // Ne permet qu'un chiffre dans le nombre de la voiture (A améliorer ?)
-	       || atoi(nums) < 0
-	       || atoi(nums) >= game_nb_pieces(g)){
-	  printf("Raté ! Veuillez entrer un numéro entre 0 et %d : ", game_nb_pieces(g)-1);
-	}
-
-	numi = atoi(nums);
-
-	
-        printf("Veuillez entrer la direction du déplacement: ");
-
-	while(fgets(direc, 10, stdin) == NULL
-	      || strcmp(direc, "UP\n\0")!= 0
-	      || strcmp(direc, "DOWN")!= 0
-	      || strcmp(direc, "LEFT")!= 0
-	      || strcmp(direc, "RIGHT")!= 0){
-
-	  printf("Direction incorrecte. Veuillez entrer UP/DOWN/LEFT/RIGHT.\n");
-	}
-
-	if(strcmp(direc, "UP")){
-	    direci = 0;
-	} else if(strcmp(direc, "LEFT")){
-	    direci = 1;
-	} else if(strcmp(direc, "DOWN")){
-	    direci = 2;
-	} else {
-	    direci = 3;
-	}
-
-	printf("Veuillez entrer la distance du déplacement: ");
-
-	// TODO - Vérifier entrées utilisateur
-	while(fgets(dists, 4, stdin) == NULL
-	       || (!isdigit(dists[0]) || dists[1] != '\n') // Ne permet qu'un chiffre dans le nombre de la distance (A améliorer ?)
-	       || atoi(dists) < 0
-	       || atoi(dists) >= game_nb_pieces(g)){
-
-	  printf("Distance incorrecte. Veuillez entrer une distance de déplacement entre 1 et %d: ", TAILLE_PLATEAU);
-	  
-	}
-	    
-        disti= atoi(dists);
-            
+       
         play_move(g, numi, direci, disti);
             
         afficher(g);
