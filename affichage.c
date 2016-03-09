3d074d941b8f0db20a6667a99a0de6cc27f1b399
Jonathan Besse <jbesse@emi.u-bordeaux1.fr>
2016-03-09 13:07:26 +0100

    modification de la creation de l'affichage

--- affichage.c
+++ affichage.c
@@ -2,16 +2,18 @@
 
 //génére l'affichage dans la console
 void afficher(cgame g){
+  int width=g->width;
+  int height= g->height;
     // Crée en mémoire un tableau rempli de -1 par défaut
-    int tab [TAILLE_PLATEAU][TAILLE_PLATEAU];
-    for(int i=0; i < TAILLE_PLATEAU ; i++){
-        for(int j=0; j < TAILLE_PLATEAU ; j++){
+    int tab [width][height];
+    for(int i=0; i < width ; i++){
+        for(int j=0; j < height ; j++){
             tab[i][j]= -1;
         } 
     }
    
    // Met les voitures à leurs place dans le tableau
-    for (int l = 0 ; l < game_nb_pieces(g) ; l++){
+    /*  for (int l = 0 ; l < game_nb_pieces(g) ; l++){
          int taille =0;
 	 if(is_horizontal(game_piece(g, l))){
              taille = get_width(game_piece(g, l));
@@ -28,18 +30,30 @@ void afficher(cgame g){
                 tab[get_y(game_piece(g, l)) +k][get_x(game_piece(g, l))]=l;
          }
        }
-    }
+       }*/
+
+
     
-     for(int i = TAILLE_PLATEAU -1; i >= 0; i--){
+    for (int l = 0 ; l < game_nb_pieces(g) ; l++){
+      int pieceW=  get_width(game_piece(g, l));
+      int pieceH= get_height(game_piece(g, l));   
+      for (int k =0; k< pieceW; k++){
+	tab[get_y(game_piece(g, l))+k][get_x(game_piece(g, l))]=l;
+      }
+      for(int j= 0; j<pieceH; j++){
+	tab[get_y(game_piece(g, l))][get_x(game_piece(g, l)) +j]=l;
+      }
+      
+     for(int i = width -1; i >= 0; i--){
         printf("   ");
 	
-        for(int j=0; j < TAILLE_PLATEAU ; j++){
+        for(int j=0; j < height ; j++){
             printf("****");
         }
 	
         printf("*\n%d: |", i);
 	
-        for(int j=0; j < TAILLE_PLATEAU ; j++){
+        for(int j=0; j < height ; j++){
             if(tab[i][j]==-1){
                 printf("   |");
             }else{
@@ -53,13 +67,13 @@ void afficher(cgame g){
     }
     printf("   *");
     
-    for(int i=0; i < TAILLE_PLATEAU ; i++){
+    for(int i=0; i < width ; i++){
      printf("****");
     }
     
     printf("\n  ");
     
-    for(int i = 0; i < TAILLE_PLATEAU ; i++){
+    for(int i = 0; i < width ; i++){
       printf("   %d", i);
     }
     
