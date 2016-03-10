f634d98037618f743c2e32f514da57aebb3c36d9
Jonathan Besse <jbesse@emi.u-bordeaux1.fr>
2016-03-10 11:40:57 +0100

    nouveau fichier

---
+++ mygame.h
@@ -0,0 +1,12 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+#include <stdbool.h>
+#include "piece.h"
+
+/**
+ * @brief selectionne le game_over correspondant au game choisi.
+ * @return true if the piece find he exit
+*/
+bool game_over(cgame g, int jeu);
+
+#endif
